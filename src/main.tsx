a51ad12c7629ecb1fa5e2eab43e47dcb83833c05
Evan Zhang <zhangevan82@gmail.com>
2026-04-13 11:17:30 -0500

    Update fix for white page

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
-import { Elements } from '@stripe/react-stripe-js';
-import { stripePromise } from './services/stripe';
 import App from './App';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Elements stripe={stripePromise}>
-      <HashRouter>
-        <App />
-      </HashRouter>
-    </Elements>
+    <HashRouter>
+      <App />
+    </HashRouter>
   </StrictMode>,
 );
